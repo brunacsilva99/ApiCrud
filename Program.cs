d79bce692e77ad3a0086479b208c07b610c7624e
Bruna Caroline Silva <bruna.caroline.silva@accenture.com>
2025-08-28 18:23:23 -0300

    Adjusts for deploy

--- Program.cs
+++ Program.cs
@@ -12,7 +12,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+    c.RoutePrefix = string.Empty; // Faz com que o Swagger seja exibido na raiz "/"
+});
 
 app.UseHttpsRedirection();
 
